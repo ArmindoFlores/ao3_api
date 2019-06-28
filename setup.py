ba1072c12ad7eb1f1323d10bba078a4f486d4a14
Francisco Rodrigues <francisco.rodrigues0908@gmail.com>
2019-06-28 11:40:19 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ao3-api",
-    version="0.0.1",
+    version="0.0.2",
     author="Francisco Rodrigues",
     author_email="francisco.rodrigues0908@gmail.com",
     description="An unofficial AO3 (archiveofourown.org) API",
-    download_url="https://github.com/ArmindoFlores/ao3_api/archive/ao3_api_v_01.tar.gz",
+    download_url="https://github.com/ArmindoFlores/ao3_api/archive/ao3_api_v_02.tar.gz",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ArmindoFlores/ao3_api",
