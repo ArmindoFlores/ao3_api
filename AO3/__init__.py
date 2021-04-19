de906ef3172434653f25a5823c59c7bf2aebf71f
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2021-04-19 17:41:07 +0100

    Changed version number and added binaries

--- AO3/__init__.py
+++ AO3/__init__.py
@@ -7,4 +7,4 @@ from .session import GuestSession, Session
 from .users import User
 from .works import Work
 
-VERSION = "2.0.8"
+VERSION = "2.1.0"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ao3-api",
-    version="2.0.8",
+    version="2.1.0",
     author="Francisco Rodrigues",
     author_email="francisco.rodrigues0908@gmail.com",
     description="An unofficial AO3 (archiveofourown.org) API",
