9aa44cecca542220a9e6d3eabe92556325a68f1e
encars28 <93652612+encars28@users.noreply.github.com>
2022-12-26 20:07:48 +0100

    Create get_work_or_series_from_banner
    This function returns a Series or a Work depending on the banner you pass.

--- AO3/common.py
+++ AO3/common.py
@@ -7,6 +7,17 @@ def __setifnotnone(obj, attr, value):
     if value is not None:
         setattr(obj, attr, value)
 
+def get_work_or_series_from_banner(banner):
+    from .series import Series
+
+    a = banner.h4.find_all("a")[0]
+
+    if "/series" in a['href']:
+        seriesid = a['href'].split("/")[-1]
+        return Series(seriesid)
+
+    return get_work_from_banner(banner)
+
 def get_work_from_banner(work):
     #* These imports need to be here to prevent circular imports
     #* (series.py would requite common.py and vice-versa)
